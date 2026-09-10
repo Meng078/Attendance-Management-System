1f390c79652890d03a387675370ace30429a628d
孟浩然 <17198325+meng-han-han@user.noreply.gitee.com>
2026-09-10 09:43:45 +0800

    添加了一些内容

--- jeecgboot-vue3/src/views/demo01/helloworld/index.vue
+++ jeecgboot-vue3/src/views/demo01/helloworld/index.vue
@@ -1,12 +1,36 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div>hello world</div>
+    <div>
+        hello world
+    </div>
 </template>
-
+<!--<script src=http://localhost:3101/src/views/super/airag/aiapp/chat/js/chat.js id="e7e007dd52f67fe36365eff636bbffbd"></script>
+ <script>
+    createAiChat({
+       appId:"2097851753539944449",
+       shareToken:"5cce0ddcf8784104b2e7455424b8c7bb",
+       // 支持top-left左上, top-right右上, bottom-left左下, bottom-right右下
+       iconPosition:"bottom-right"
+    })
+ </script>-->
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+onMounted(() => {
+    const script = document.createElement('script')
+    script.src = 'http://localhost:3101/src/views/super/airag/aiapp/chat/js/chat.js'
+    script.id = 'e7e007dd52f67fe36365eff636bbffbd'
+    script.onload = () => {
+        createAiChat({
+            appId: "2097851753539944449",
+            shareToken: "5cce0ddcf8784104b2e7455424b8c7bb",
+            // 支持top-left左上, top-right右上, bottom-left左下, bottom-right右下
+            iconPosition: "bottom-right"
+        })
+    }
+    document.body.appendChild(script)
+})
 
 </script>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
